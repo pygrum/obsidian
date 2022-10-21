cc081878f89737399b63a40649be5fd2da9f5f34
Gambit-99 <sec.gambit@gmail.com>
2022-10-21 19:20:32 +0100

    fixed bug (endpoint overwriting) by making a copy of object passed in replace_dicts file

--- main.py
+++ main.py
@@ -24,10 +24,12 @@ def start (target_file, payload_file, args):
     for target in target_data["targets"]:
         for endpoint in target["endpoints"]:
             for payload in payloads:
-                if (payload["id"] in args.fp) or (target["id"] in args.ft):
+                if ((payload["id"] in args.fp) or (target["id"] in args.ft)) or ((args.fp == []) and (args.ft == [])):
+                    call = processor.process(target, payload, endpoint, replace_string, proxies)
+                    log (call)       # payloads|>----endpoint|>----target
+                else:
                     continue
-                call = processor.process(target, payload, endpoint, replace_string, proxies)
-                log (call)       # payloads|>----endpoint|>----target
+
 
 
 parser = argparse.ArgumentParser()
@@ -36,8 +38,8 @@ subparsers = parser.add_subparsers()
 run = subparsers.add_parser("run")
 run.add_argument("--targets", metavar="", default="targets.json", help="specify json file full of targets (use targets.json as writing reference)")
 run.add_argument("--payloads", metavar="", default="payloads.json", help="specify json file full of payloads (use payloads.json as writing reference)")
-run.add_argument("-fp", metavar="",nargs="*", default=[], help="do NOT run payloads that match the IDs specified")
-run.add_argument("-ft", metavar="",nargs="*", default=[], help="do NOT test against targets that match the IDs specified")
+run.add_argument("-fp", metavar="",nargs="*", default=[], help="run payloads that match the IDs specified")
+run.add_argument("-ft", metavar="",nargs="*", default=[], help="test against targets that match the IDs specified")
 
 args = parser.parse_args()
 start(args.targets, args.payloads, args)
--- processor.py
+++ processor.py
@@ -1,30 +1,20 @@
 #!/usr/bin/env python3
-"""
-Log4Shell is a vulnerability disclosed in December 2021, concerning the Apache logging library Log4j.
-It allowed an attacker to execute code via a logged parameter in a HTTP(S) request, such as a header, or post data.
-An attacker would pass a payload through a logged parameter causing the server to access their LDAP server, and evaluate
-the Java code hosted by the attacker.
-
-To test a website for Log4Shell, it would be wise to test multiple endpoints (pages on the site), as the library
-is likely to be used in multiple places. It should also be tested on multiple endpoints, both of which can be 
-specified by the user in the targets.json file.
-"""
 
 import json, requests
 from requests.exceptions import ConnectionError
 
 def replace_dicts ( obj, rep_str, payload ):
+    new_obj = {}
     # Replaces each replace-string ('i.e. PAYLOAD') in all dict keys' values with the payload.
     for key, value in obj.items():
-        value = value.replace( rep_str, payload )
-        obj[key] = value
-    return obj
+        new_val = value.replace( rep_str, payload )
+        new_obj[key] = new_val
+    return new_obj
 
-def process (target, payload, endpoint, rep_str, proxies): 
+def process (target, payload, endpoint, rep_str, proxies):
     """
     This function constructs the request based off of the values defined in the targets file.
     """
-
     # Next block defines the request details
     # and replaces wherever the payload replace string ('i.e. PAYLOAD') is with the actual payload
 
@@ -33,12 +23,12 @@ def process (target, payload, endpoint, rep_str, proxies):
     URL = URL.replace(rep_str, payload["payload"]) # Replace special string with payload if required 
 
     # Replace any request params with payload (if specified)
+    # Making copy so as not to overwrite actual endpoint object
     PARAMS = replace_dicts( endpoint["params"], rep_str, payload["payload"])   
-
-    # Replace any request headers with payload (if specified) 
+    # Replace any request headers with payload (if specified)
     HEADERS = replace_dicts( endpoint["headers"], rep_str, payload["payload"])
     COOKIES = endpoint["cookies"]
-
+ 
     try:
         # Replace any request post data with payload (if specified)
         DATA = replace_dicts( endpoint["post"]["data"], rep_str, payload["payload"] )
@@ -49,7 +39,7 @@ def process (target, payload, endpoint, rep_str, proxies):
         JSON = replace_dicts( endpoint["post"]["json"], rep_str, payload["payload"] )
     except:
         JSON = {}
-
+        
     
 
     # fill request obj with config params
